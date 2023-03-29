d3e3289ab115b3414a8d4bb220e1633a522bc1ac
Kenta Shinkai <28914836+5h1nk41@users.noreply.github.com>
2023-03-29 14:36:58 +0900

    Update domain_info_retriever.py

--- domain_info_retriever.py
+++ domain_info_retriever.py
@@ -1,51 +1,45 @@
-
 import streamlit as st
 import dns.resolver
-import subprocess
+import requests
 import openai
 import pandas as pd
 import os
+import re
 
 # GPT-3 APIキーを設定します。環境変数から取得してください。
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # WHOIS情報の取得
 @st.cache_data
-def get_whois_info(domain):
+def get_whois_info(api_key, domain):
     try:
-        command = f"whois {domain}"
-        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        return output.decode()
+        url = f"https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey={api_key}&domainName={domain}&outputFormat=JSON"
+        response = requests.get(url)
+        return response.json()
     except Exception as e:
         st.error(f"WHOIS情報の取得中にエラーが発生しました: {e}")
-        return ""
-
-# WHOIS情報の結果をパースして辞書→Pandas DataFrameに変換
-@st.cache_data
-def parse_whois_data(whois_raw):
-    whois_data = {}
+        return {}
+    
+# WHOIS情報の結果をパースしてステータスを取得し、Pandas DataFrameに変換
+def parse_whois_data(whois_data):
     desired_keys = [
-        "Domain Name",
-        "Registrar",
-        "Registrar URL",
-        "Registrar Abuse Contact",
-        "Registrar Abuse Contact",
-        "Updated Date",
-        "Creation Date",
-        "Registrar Registration Expiration Date",
-        "Domain Status",
-        "Registrant Email",
-        "Admin Email",
-        "DNSSEC",
+        "domainName",
+        "createdDateNormalized",
+        "updatedDateNormalized",
+        "expiresDateNormalized",
+        "status",
     ]
 
-    for line in whois_raw.split("\n"):
-        if ":" in line:
-            key, value = [part.strip() for part in line.split(":", 1)]
-            if key in desired_keys:
-                whois_data[key] = value
-    return whois_data
+    whois_info = {}
+    if 'WhoisRecord' in whois_data and 'registryData' in whois_data['WhoisRecord']:
+        registry_data = whois_data['WhoisRecord']['registryData']
+
+        for key in desired_keys:
+            if key in registry_data:
+                value = registry_data[key]
+                whois_info[key] = value
+
+    return pd.DataFrame(whois_info.items(), columns=["Key", "Value"])
 
 # DNS情報の取得
 @st.cache_data
@@ -92,48 +86,48 @@ def get_dns_info(domain):
 
 # GPT-3を使って情報を解説する
 @st.cache_data
-def get_gpt3_summary(whois_info, dns_info):
-    formatted_whois = "\n".join([f"{key}: {value}" for key, value in whois_info.items()])
+def get_gpt3_summary(whois_raw, dns_info):
     formatted_dns = "\n".join([f"{key}: {value}" for key, value in dns_info.items()])
 
     prompt = (
-        f"WHOIS情報とDNS情報の要約と解説を日本語で提供してください。\n\n"
-        f"WHOIS情報:\n{formatted_whois}\n\n"
+        f"ドメインのWHOISステータスとDNS情報の要約と解説を日本語で提供してください。\n\n"
+        f"WHOISステータス:\n{whois_raw}\n\n"
         f"DNS情報:\n{formatted_dns}\n\n"
     )
 
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=prompt,
         max_tokens=1000,
         n=1,
         stop=None,
-        temperature=0.1,
+        temperature=0.5,
     )
 
     return response.choices[0].text.strip()
 
+# メイン
 st.title("Domain Information Retriever")
 
 domain = st.text_input("Enter a domain name:")
 
 if domain:
+    api_key = "at_BW8qU6pHDtKEqGbPU4DFMkZEkHiqf"
     with st.spinner("Retrieving WHOIS information..."):
-        whois_raw = get_whois_info(domain)
-        whois_data = parse_whois_data(whois_raw)
-        whois_df = pd.DataFrame(whois_data.items(), columns=["Key", "Value"])
+        whois_data = get_whois_info(api_key, domain)
+        whois_status_df = parse_whois_data(whois_data)
 
     with st.spinner("Retrieving DNS information..."):
         dns_info = get_dns_info(domain)
 
     with st.spinner("Generating summary and explanation..."):
-        summary = get_gpt3_summary(whois_data, dns_info)
+        summary = get_gpt3_summary(whois_status_df.to_string(index=False, header=False), dns_info)
 
-    st.header("WHOIS Information")
-    st.write(whois_df)
+    st.header("WHOIS Status")
+    st.write(whois_status_df)
 
     st.header("DNS Information")
     st.write(dns_info)
 
     st.header("Summary and Explanation")
-    st.markdown(f"> {summary}")
+    st.markdown(f"{summary}")
